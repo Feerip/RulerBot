008df58298de9869c5cd8755f68a89fff2f822c7
Phillip Smith <feerip@gmail.com>
2022-04-25 16:49:40 -0700

    Fixed bad folder names and added test project

---
+++ RulerBotTests/CoCAPITests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using RulerBot;
+
+namespace RulerBotTests
+{
+    [TestClass]
+    public class CoCAPITests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
